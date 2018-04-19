4353ece5ab8146f7cf7f3e50bdd8764b88758e8c
jday03 <E!n$t31n>
2018-04-18 17:51:45 -0700

    Implemented TCP server

--- server.c
+++ server.c
@@ -1,7 +1,3 @@
-//
-// Created by juliann on 4/16/2018.
-//
-
 
 
 #include<stdio.h>
@@ -11,9 +7,42 @@
 int main() {
 
     int socketIdentifier;
-    sockaddr_in serverDetails;
+    const int port = 8080;
+    int opt = 1;
+    sockaddr_in address;
+    
 
     socketIdentifier = socket(AF_INET, SOCK_STREAM, 0);
-    serverDetails.sin_addr.s_addr = inet_addr("74.125.235.20");
+
+    if(socketIdentifier < 0){
+        std::cout << "socket creation failure" << std::endl;
+
+        exit(1);
+}
+
+
+    if(setsockopt(socketIdentifier, SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT,opt, sizeof(opt) ) < 0){
+
+        std::cout << "socket opt failure" << std::endl;
+        exit(1);
+    }
+
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( port);
+
+   if( bind(socketIdentifier, (struct sockaddr *)& address,sizeof(address)) < 0){
+       std::cout << "bind failure" << std::endl;
+       exit(1);
+   }
+
+
+    if( listen(socketIdentifier,1) < 0) {
+        std::cout << "listen failure" << std::endl;
+        exit(1);
+    }
+
+    new_socket = accept(server_fd, (struct sockaddr *)&address,sizeof(address));
 
 }
