c1c29b1f5d3aa3620905bbeb1c58d278f01571ae
jday03 <E!n$t31n>
2018-04-20 14:31:11 -0700

    worked on more TCP stuff

--- client_cpp_tcp.cpp
+++ client_cpp_tcp.cpp
@@ -5,12 +5,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-
+#include <iostream>
 #define PORT 8080
 
 
 
-void askUser(int *port,char* address, char* command ){
+void askUser(int *port, char* address, char* command ){
   //  printf("Enter server name or IP address: ");
   //  scanf("%s", address);
 
@@ -23,28 +23,75 @@ void askUser(int *port,char* address, char* command ){
 
 }
 
+FILE* receiveFile(int socketDescriptor, char* fileName){
+    FILE * newFile= fopen(fileName,"w");
+
+    int messagesRemaining;
+    const int bufferSize= 1024;
+    const int firstBufferSize = bufferSize+1;
+    char* buffer=(char*)malloc(firstBufferSize*sizeof(char));
+
+
+        if(recv(socketDescriptor,buffer,bufferSize*sizeof(char) , 0) < 0){
+            printf ("receive failure\n");
+            exit(1);
+        }
+
+    messagesRemaining = (int)buffer[0];
+    char* fullBuffer = (char*)malloc(messagesRemaining*bufferSize*sizeof(char));
+
+    for(int i = 0;i < bufferSize;++i ){
+        fullBuffer[i]=buffer[i+1];
+    }
+
+    free(buffer);
+    buffer = (char*)malloc(bufferSize*sizeof(char));
+
+    --messagesRemaining;
+    while(messagesRemaining > 0){
+
+        if(recv(socketDescriptor,buffer,bufferSize*sizeof(char) , 0) < 0){
+            printf ("receive failure\n");
+            exit(1);
+        }
+
+        for(int i = 0;i < bufferSize;++i ){
+            fullBuffer[i]=buffer[i];
+        }
+
+        --messagesRemaining;
+    }
+
+    //}while()
+    fprintf(newFile,"%s",fullBuffer);
+    return newFile;
+}
+//assuming that buffer is empty
+
+
+char* parseCommand(char*command){
+
+    int specialCharIndex = strstr(command,">");
+
+}
 
 
 int main() {
 
     int* port = malloc(sizeof(int));
-    *port = 8080;
     char* addressInput = malloc(100* sizeof(char));
     char* command = malloc(200* sizeof(char));
-   askUser(port,addressInput,command);
-    printf("sending: %s \n", command);
+    askUser(port,addressInput,command);
+    *port = 8080;
 
     int socketIdentifier;
-    int opt = 1;
     struct sockaddr_in address;
 
     memset(&address, '0', sizeof(address));
-
     socketIdentifier = socket(AF_INET, SOCK_STREAM, 0);
 
     if(socketIdentifier < 0){
         printf ("socket creation failure\n");
-
         exit(1);
     }
 
@@ -77,8 +124,6 @@ int main() {
 
     }
 
-    char basic [3300];
-    FILE* file= malloc(200*sizeof(FILE));
 
 /*    if(recv(socketIdentifier,  &file, 200*sizeof(file), 0) < 0){
         printf ("receive failure\n");
--- server_cpp_tcp.cpp
+++ server_cpp_tcp.cpp
@@ -8,6 +8,62 @@
 
 
 
+
+
+
+
+void sendFile(FILE* file, int socketDescriptor){
+    // one char for telling if done or not
+    const int sendSize = 1024;
+    const int firstBufferSize = sendSize+1;
+    char* firstBuffer = (char*) malloc((sendSize+1) * sizeof(char));
+    char* buffer = (char*) malloc((sendSize) * sizeof(char));
+    fseek(file,0,SEEK_END);
+    long size = ftell(file);
+    rewind(file);
+
+    int messages = size/sendSize;
+    if(messages % sendSize >0){
+        ++messages;
+    }
+
+    firstBuffer[0]=(char) messages;
+    for (int messageCount = 1; messageCount < sendSize; ++messageCount) {
+        firstBuffer[messageCount] = (char) fgetc(file);
+        if (feof(file)) {
+            messageCount = sendSize;
+
+        }
+    }
+
+    if (send(socketDescriptor, firstBuffer, (size_t) (firstBufferSize* sizeof(char)), 0) < 0) {
+        printf("\nsend Failed \n");
+        exit(1);
+    }
+
+
+    messages--;
+    while(messages > 0) {
+
+        for (int messageCount = 1; messageCount < sendSize; ++messageCount) {
+            buffer[messageCount] = (char) fgetc(file);
+            if (feof(file)) {
+                messageCount = sendSize;
+
+            }
+        }
+
+        if (send(socketDescriptor, buffer, (size_t) (sendSize* sizeof(char)), 0) < 0) {
+            printf("\nsend Failed \n");
+            exit(1);
+        }
+        messages--;
+
+    }
+
+}
+
+
 int main() {
 
     int socketIdentifier;
